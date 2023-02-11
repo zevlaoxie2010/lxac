cfa9803e46cb7f3e3ff26d05a53b81cd9887e1de
zevlaoxie2010 <95100738+zevlaoxie2010@users.noreply.github.com>
2023-02-11 11:18:48 +0800

    Create c2.js

---
+++ c2.js
@@ -0,0 +1,6 @@
+var _VER = '0.1.3'
+//测试产品
+logger.setConsole(true);
+logger.setFile("./logs/lxac2.log", 3);
+logger.setTitle("lxac2")
+var conf = data.openConfig("./plugins/lxac/config.json", "json", "{}");
