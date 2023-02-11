9629927ce7573fd3cceb4c00b4a492f1ad0c8672
zevlaoxie2010 <95100738+zevlaoxie2010@users.noreply.github.com>
2023-02-11 11:20:01 +0800

    Update c2.js

--- c2.js
+++ c2.js
@@ -4,3 +4,9 @@ logger.setConsole(true);
 logger.setFile("./logs/lxac2.log", 3);
 logger.setTitle("lxac2")
 var conf = data.openConfig("./plugins/lxac/config.json", "json", "{}");
+
+
+log('[lxac] ============================================');
+log('[lxac] lxac插件已装载  当前版本：' + _VER);
+log('此为测试程序 laoxienet内部');
+log('[lxac] ============================================');
